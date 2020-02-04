c3642baff05e86045456e3a9db0744f8ebadf42f
itstrivellato <luca.trivellato@tecnicosuperiorekennedy.it>
2020-02-04 16:01:17 +0100

    ForeignKey dipendentiDao

--- src/main/java/com/bert/test/test/dao/DipendentiDao.java
+++ src/main/java/com/bert/test/test/dao/DipendentiDao.java
@@ -5,7 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.ForeignKey;
 
 import lombok.Data;
 
@@ -34,6 +37,8 @@ public class DipendentiDao {
 	@Column(name = "address")
 	private String address;
 	
-	@Column(name = "city")
-	private int city;
+	@ManyToOne
+	@JoinColumn(name = "city", foreignKey = @ForeignKey(name = "dipendenti_ibfk_1"))
+	private CittaDao city;
+
 }
